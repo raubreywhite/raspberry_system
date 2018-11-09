ba782462d919d4ad13085942affa65a4235b3c31
Richard White <w@rwhite.no>
2018-11-09 20:17:06 +0200

    logs

--- py/camera.py
+++ py/camera.py
@@ -1,5 +1,10 @@
 import picamera
 import time
+import json
+import os
+
+storage_path = "/var/www/dist/logs/camera.json"
+timeStart = time.strftime("%Y-%m-%d %H:%M:%S")
 
 camera = picamera.PiCamera()
 camera.resolution = (2592, 1944)
@@ -7,7 +12,7 @@ camera.resolution = (2592, 1944)
 camera.start_preview()
 camera.annotate_background = picamera.Color('blue')
 camera.annotate_foreground = picamera.Color('yellow')
-camera.annotate_text = "Time: %s" % time.strftime("%H:%M:%S")
+camera.annotate_text = "Time: %s" % timeStart
 camera.annotate_text_size = 50
 time.sleep(5)
 
@@ -15,3 +20,27 @@ camera.capture('/var/www/dist/camera/image.jpg')
 camera.stop_preview()
 
 camera.close()
+
+data_base = None
+if os.path.exists(storage_path):
+    with open(storage_path,'r') as f:
+        try:
+            data_base = json.load(f)
+            print('loaded that: ',data_base)
+        except Exception as e:
+            print("got %s on json.load()" % e)
+
+
+if data_base is None:
+    print('Create new dataset')
+    data_base = [ ]
+
+data_base.append(timeStart)
+
+data_base_json = json.dumps(data_base)
+
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile)
+
+
+
