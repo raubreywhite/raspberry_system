85665c4cb467aba4fb6eec04dd92b7e844ed94b0
Richard White <w@rwhite.no>
2018-11-09 14:46:11 +0200

    Update py/camera.py

--- py/camera.py
+++ py/camera.py
@@ -3,9 +3,9 @@ import time
 
 camera = picamera.PiCamera()
 camera.resolution = (2592, 1944)
-#camera.annotate_text = "Time: " + time.strftime("%H:%M:%S")
 
 camera.start_preview()
+camera.annotate_text = "Time: " + time.strftime("%H:%M:%S")
 time.sleep(5)
 camera.capture('/var/www/raspberry_reactjs/camera/image.jpg')
 camera.stop_preview()
